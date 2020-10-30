9ff0a4632ed25f36a8b8b11d8482254d9d62e85f
William DeNiro <deniro.12@osu.edu>
2020-10-29 20:23:13 -0400

    Added group stage standing and images

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import  Home  from './Home'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-dark">
      <Home/>
     </div>
   );
--- src/Home.js
+++ src/Home.js
@@ -20,6 +20,7 @@ import React, { Component } from 'react'
            },
            })
            .then((response)=>{
+             console.log(response)
               this.setState({ groups : response.data.standings});
             })
            .catch((error)=>{
@@ -30,17 +31,54 @@ import React, { Component } from 'react'
 
     printSchedule(){
 
-      console.log(this.state.groups);
+    console.log(this.state.groups);
+    
+     var groups  = this.state.groups.map((group) =>
+       group.type === "TOTAL" ?
+         <div class="col-md-6">
+            <div class="card p-4 m-4">   
+              <h3  class="card-title">{group.group.replace('_'," ")}</h3>
+              <table>
+                <th>Position</th>
+                <th></th>
+                <th>Team</th>
+                <th><nobr>Points</nobr></th>
+                <th>GP</th>
+                <th>GF</th>
+                <th>GA</th>
+
+              { 
+                  group.table.map((table) => 
+                  <tr>
+                    <td class="card-text "> { table.position + ". "} </td>
+                    <td class="card-text"><img src={table.team.crestUrl} width="40" height="20" /></td>
+                    <td class="card-text">  { table.team.name} </td> 
+                    <td class="card-text p-3"> { table.points} </td>   
+                    <td class="card-text p-3"> { table.playedGames} </td>   
+                    <td class="card-text p-3"> { table.goalsFor} </td>   
+                    <td class="card-text p-3"> { table.goalsAgainst} </td>   
+
+
+                  </tr>  
+                  )        
+                }
+            </table>
+            </div>
+          </div>
+       : ""
+      );
+
+      return (
+        <div class="row"> { groups }</div>
+      )
       
     }
     
 
-  
-
     render() {
         return (
         <div>
-            <h1>Champions League 2020</h1>
+            <h2 class="text-light display-4 ">Champions League 2020 Standings</h2>
             <div class="container">
                 {this.printSchedule()}
              </div>
